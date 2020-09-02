cfbbd154c3895bfcce2a15eb08dc94c225a6bbfe
John P. Dickerson <john@cs.umd.edu>
2020-09-01 20:17:30 -0400

    Lecture 01 files added

--- lecs/update_decks.sh
+++ lecs/update_decks.sh
@@ -5,7 +5,7 @@ set -o errexit
 
 # Copy all CMSC320 lectures (pptx and pdf) from private into public repo
 privatedir=~/code/cmsc320-fall2020-private/lecs/
-publicdir=~/code/cmsc320-fall2020-website/lecs/
+publicdir=~/code/cmsc320-website/lecs/
 cp -v ${privatedir}/cmsc320_f2020_lec*pptx ${publicdir}/
 cp -v ${privatedir}/cmsc320_f2020_lec*pdf ${publicdir}/
 
